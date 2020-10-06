93e61ecab6e40f73ddbde7185032f651858b019e
GabrielLinharesBozzon <44560145+GabrielLinharesBozzon@users.noreply.github.com>
2020-10-06 12:08:02 -0300

    exer6

--- Estrutura_Sequencial/ex5.py
+++ Estrutura_Sequencial/ex5.py
@@ -1,1 +1,4 @@
 #-*-coding:utf8-*-
+#Faça um Programa que converta metros para centímetrosn= .
+n=int(input("Qual o valor :\n"))
+pritn("O valor da conversão é:",n*100)
---
+++ Estrutura_Sequencial/ex6.py
@@ -0,0 +1,3 @@
+#-*-coding:utf8-*-
+#6)Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
+n1=int(input("Digite o raio"))
