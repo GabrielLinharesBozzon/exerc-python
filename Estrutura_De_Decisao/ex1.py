f1d392bfe863f7b8808feac79f0e93ffaf50c0d1
GabrielLinharesBozzon <44560145+GabrielLinharesBozzon@users.noreply.github.com>
2020-11-25 10:54:17 -0300

    #

--- Estrutura_De_Decisao/ex1.py
+++ Estrutura_De_Decisao/ex1.py
@@ -1,7 +1,7 @@
-# -*-coding:utf8-*-
-# Faça um Programa que peça dois números e imprima o maior deles.
-n1 = int(input("Digite o primeiro valor:\n"))
-n2 = int(input("Digite o segundo valorr:\n"))
+#-*-coding:utf8-*-
+#Faça um Programa que peça dois números e imprima o maior deles.
+n1 =int(input("Digite o primeiro valor:\n"))
+n2 =int(input("Digite o segundo valorr:\n"))
 if n1 <= n2:
     print(n1)
 elif n2 <= n1:
