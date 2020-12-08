7c8ead435066c6f581dbce9044c07651e38ff705
Gabriel L. Bozzon <44560145+GabrielLinharesBozzon@users.noreply.github.com>
2020-12-08 10:20:12 -0300

    8122020

--- Estrutura_De_Decisao/ex11.py
+++ Estrutura_De_Decisao/ex11.py
@@ -9,4 +9,14 @@
 #o percentual de aumento aplicado;
 #o valor do aumento;
 #o novo salário, após o aumento.
-S=input("Salario ")
+S=float(input("Digite seu salario:\n"))
+if S <= 280:
+    print("Seu salaraio é de:",S,"\nO aumento foi de 20%\nO acrecimo foi de:",((S/100)*20),"R$\nTotalizando:",(S+((S/100)*20)))
+elif S >=280 and S<=700:
+    print("Seu salaraio é de:",S,"\nO aumento foi de 15%\nO acrecimo foi de:",((S/100)*15),"R$\nTotalizando:",(S+((S/100)*15)))
+elif S >= 700 and S <= 1500:
+    print("Seu salaraio é de:",S,"\nO aumento foi de 10%\nO acrecimo foi de:",((S/100)*10),"R$\nTotalizando:",(S+((S/100)*10)))   
+elif S >= 1500:
+    print("Seu salaraio é de:",S,"\nO aumento foi de 5%\nO acrecimo foi de:",((S/100)*5),"R$\nTotalizando:",(S+((S/100)*5)))
+else:
+    print("Valor não aceito")    
